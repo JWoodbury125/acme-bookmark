b2cfa5ed6827a535459abe4c945d66d294467bd4
Jennifer Woodbury <jenann125@yahoo.com>
2022-02-12 19:14:33 -0500

    Update with create bookmark functionality

--- server.js
+++ server.js
@@ -2,24 +2,56 @@ const { db, Bookmark, Category } = require('./db')
 const express = require ('express')
 const app = express()
 
+app.use(express.urlencoded({ extended:false } ))
+
 app.get('/', (req, res) => res.redirect('/bookmarks'))
+
+app.post('/bookmarks/:categoryId', async (req, res, next) => {
+    try{
+        const bookmark = await Bookmark.create(req.body)
+        res.redirect(`/category/${bookmark.categoryId}`)
+    }
+    catch(ex){
+        next(ex)
+    }
+})
+
+
 app.get('/bookmarks', async (req, res, next) => {
     try{
         const bookmarks = await Bookmark.findAll({
             include: [ Category ]
         })
+        const categories = await Category.findAll()
         const html = bookmarks.map( bookmark =>{
             return `
                 <div> ${bookmark.name}  <a href='/category/${bookmark.categoryId}'>${bookmark.category.name}</a></div>
             `
         }).join('')
+        console.log(categories)
+
         res.send(`
             <html>
                 <head> <title> Acme_Bookmarks </title>
                 </head>
                 <body>
                     <h1> Acme Bookmarks </h1>
+                    <div>
+                        <form method='POST' action='/bookmarks/:create_bookmark'>
+                            <input name='name' placeholder='Enter Name'/>
+                            <select name='categoryId'>
+                                ${categories.map(category => {
+                                    return `
+                                        <option value='${category.id}'>${ category.name }</option>
+                                    `
+                                    }).join('')
+                                } 
+                            </select>
+                            <button> Create Bookmark </>
+                        </form>
+                    </div>
                     ${html}
+
                 </body>
             </html>
         `)
@@ -31,8 +63,26 @@ app.get('/bookmarks', async (req, res, next) => {
 
 app.get('/category/:categoryId', async (req, res, next) => {
     try{
-        const categories = await Category.findByPk(req.params.categoryId)
-        res.send(categories.name)
+        const categories = await Category.findByPk(req.params.categoryId, {
+            include: [ Bookmark ]
+        })
+        const html = categories.bookmarks.map( category => {
+            return `
+               <div> ${category.name} </div>
+            `
+        }).join('')
+        res.send(`
+                <html> 
+                    <head><title>${categories.name}</title></head>
+                    <body>
+                        <a href='/'> << BACK </a>
+                        <h1>Bookmark Category *${categories.name}*  </h1>
+                        ${html}
+                    </body>
+                </html>`)
+        
+        
+        res.send(html)
     }
     catch(ex){
         next(ex)
